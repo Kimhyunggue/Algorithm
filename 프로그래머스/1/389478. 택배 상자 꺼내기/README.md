5a70db6dfcbf4b7cb87e344c31c2948ed6097585
Kimhyunggue <113527269+Kimhyunggue@users.noreply.github.com>
2025-04-24 19:20:14 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/README.md
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 택배 상자 꺼내기 - 389478 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/389478) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/389478?language=c) 
 
 ### 성능 요약
 
-메모리: 4.19 MB, 시간: 0.01 ms
+메모리: 4.2 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 24일 17:31:33
+2025년 04월 24일 19:20:12
 
 ### 문제 설명
 
--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.c
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.c
@@ -4,48 +4,12 @@
 
 int solution(int n, int w, int num) {
     int answer = 0;
-    int row, col, numrow, numcol;
-    if(n % w == 0){
-        row = n / w;
-        if(row % 2 == 0)
-            col = 0;
-        else
-            col = w;
+    int m1 = num % (2*w);
+    int m2 = ((2*w+1)-m1) % (2*w);
+    for(int i=num; i<=n; i++){
+        if(i%(2*w) == m1 || i%(2*w) == m2)
+            answer++;
     }
-    else{
-        row = n / w + 1;
-        if(row % 2 == 0)
-            col = w + 1 - n % w;
-        else
-            col = n % w;
-    }
-    if(num % w == 0){
-        numrow = num / w;
-        if(numrow % 2 == 0)
-            numcol = 0;
-        else
-            numcol = w;
-    }
-    else{
-        numrow = num / w + 1;
-        if(numrow % 2 == 0)
-            numcol = w + 1 - num % w;
-        else
-            numcol = num % w;
-    }
-    if(row % 2 == 0){
-        if(col > numcol)
-            answer = row - numrow;
-        else
-            answer = row - numrow + 1;
-    }
-    else{
-        if(col < numcol)
-            answer = row - numrow;
-        else
-            answer = row - numrow + 1;
-    }
-    
     
     return answer;
 }
